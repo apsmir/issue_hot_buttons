8001bfa74e90f0713bd8d61320742058d2f60416
smalex <ap.smir@gmail.com>
2024-04-30 20:26:43 +0300

    small fix

--- init.rb
+++ init.rb
@@ -1,30 +1,21 @@
-require 'redmine'
-require_dependency File.dirname(__FILE__) + '/lib/sql_button_patch'
-#require_dependency 'sql_button_patch'
-
-if Rails::VERSION::MAJOR < 3
-  require 'dispatcher'
-  object_to_prepare = Dispatcher
-else
-  object_to_prepare = Rails.configuration
-end
-
-object_to_prepare.to_prepare do
-  require File.dirname(__FILE__) + '/lib/issues_controller_patch.rb'
-  IssuesController.send(:include, IssueHotButtons::IssuesControllerPatch)
-end
-
-class Hooks < Redmine::Hook::ViewListener
-  render_on :view_issues_show_details_bottom,
-    :partial => 'hot_buttons/assets',
-    :layout => false
-end
-
 Redmine::Plugin.register :issue_hot_buttons do
+  Rails.logger.info 'Plugin issue_hot_buttons register'
   name 'Issue Hot Buttons'
   author 'Alexey Smirnov'
   description 'Plugin for Redmine that add buttons for often used actions to issue page'
   version '5.0'
   settings :partial => 'hot_buttons/settings'
   url 'https://github.com/apsmir/issue_hot_buttons'
+end
+
+require_dependency File.dirname(__FILE__) + '/lib/sql_button_patch'
+
+Rails.application.config.after_initialize do
+  IssuesController.send(:prepend, IssueHotButtons::IssuesControllerPatch)
+end
+
+class Hooks < Redmine::Hook::ViewListener
+  render_on :view_issues_show_details_bottom,
+    :partial => 'hot_buttons/assets',
+    :layout => false
 end
